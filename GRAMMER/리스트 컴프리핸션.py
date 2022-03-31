9b13d3623427dc96cf0486c1ac0983af80b7f073
eternalclash <suchasucha@naver.com>
2022-03-31 22:29:12 +0900

    파이썬 문법정리

---
+++ GRAMMER/리스트 컴프리핸션.py
@@ -0,0 +1,6 @@
+array = [i for i in range(20) if i%2==0]
+array = [i*i for i in range(10)]
+n=3
+m=4
+array = [[0]*m for _ in range(n)] #n,m으로 된 배열 만들기
+# _ 언더바는 변수를 무시할 떄 사용함
---
+++ GRAMMER/실수 연산.py
@@ -0,0 +1,10 @@
+a= 0.3 + 0.6
+print(a)
+if a== 0.9:
+    print(True)
+else :
+    print(False)
+
+#FALSE가 나옴 0.899999999999
+#round(element,반올림위치 -1)
+#실수형 데이터를 비교할 때 소수점 다섯 번째 자리에서 반올림한 결과가 같으면 정답
