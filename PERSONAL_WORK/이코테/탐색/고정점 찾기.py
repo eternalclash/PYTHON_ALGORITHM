81e0734af3645a8d9d894d4432acba1ab629ef6f
eternalclash <suchasucha@naver.com>
2022-04-26 10:29:37 +0900

    파일 수정

---
+++ PERSONAL_WORK/이코테/탐색/고정점 찾기.py
@@ -0,0 +1,16 @@
+
+
+array=[-15,-6,1,3,7]
+def binary_search(array,start,end):
+    if start>end:
+        return -1
+    mid=(start+end)//2
+    if array[mid]==mid:
+        return mid
+    elif array[mid]>mid:
+        return binary_search(array,start,mid-1)
+    elif array[mid]<mid:
+        return binary_search(array,mid+1,end)
+
+print(binary_search(array,0,len(array)-1))
+    
